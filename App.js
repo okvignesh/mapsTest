ad5eac4281c8e544f47df2f3bb80ef7ebe22dbd6
okvignesh <ok.vignesh@outlook.com>
2023-11-23 12:41:07 +0000

    Basic apple maps

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import {MapScreen} from './src/containers';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <MapScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
