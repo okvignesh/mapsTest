07225ae75fd8653480834053b9a8caf399ff0288
okvignesh <ok.vignesh@outlook.com>
2023-11-23 18:59:39 +0000

    used onMapReady event

--- src/containers/MapScreen/index.js
+++ src/containers/MapScreen/index.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const MapScreen = props => {
-  //   const [isMapReady, setIsMapReady] = useState(false);
+  const [isMapReady, setIsMapReady] = useState(false);
   const markers = [
     {
       latitude: '37.776257,', //37.776257,-122.4353305
@@ -26,39 +26,40 @@ const MapScreen = props => {
       <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
-        // onMapReady={() => {
-        //   setIsMapReady(true);
-        // }}
+        onMapReady={() => {
+          setIsMapReady(true);
+        }}
         region={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
-        {markers.map(thisEl => {
-          return (
-            <Marker
-              draggable
-              title={thisEl.title}
-              description={'this is the description'}
-              coordinate={{
-                latitude: parseFloat(thisEl.latitude),
-                longitude: parseFloat(thisEl.longitude),
-              }}>
-              <View
-                style={{
-                  width: 40,
-                  height: 40,
-                  backgroundColor: 'grey',
-                  borderRadius: 20,
-                  justifyContent: 'center',
-                  alignItems: 'center',
+        {isMapReady === true &&
+          markers.map(thisEl => {
+            return (
+              <Marker
+                draggable
+                title={thisEl.title}
+                description={'this is the description'}
+                coordinate={{
+                  latitude: parseFloat(thisEl.latitude),
+                  longitude: parseFloat(thisEl.longitude),
                 }}>
-                <Text>V</Text>
-              </View>
-            </Marker>
-          );
-        })}
+                <View
+                  style={{
+                    width: 40,
+                    height: 40,
+                    backgroundColor: 'grey',
+                    borderRadius: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text>V</Text>
+                </View>
+              </Marker>
+            );
+          })}
       </MapView>
     </View>
   );
